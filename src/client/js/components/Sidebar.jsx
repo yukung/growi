06dbe639e645a7efe5aff48b0dd8b55f48d6fee0
Yuki Takei <yuki@weseek.co.jp>
2019-10-03 22:31:45 +0900

    add dropdown menu

--- src/client/js/components/Sidebar.jsx
+++ src/client/js/components/Sidebar.jsx
@@ -1,6 +1,10 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
+
+import Dropdown from 'react-bootstrap/es/Dropdown';
+import MenuItem from 'react-bootstrap/es/MenuItem';
 
 import AppContainer from '../services/AppContainer';
 
@@ -18,15 +22,65 @@ class Sidebar extends React.PureComponent {
       .replace(/<\/div>$/, '');
 
     this.state = {
+      isClient: false,
+      isMenuOpened: false,
       html: innerHTMLWithoutNestedDiv,
     };
 
+    this.onToggleMenuDropdown = this.onToggleMenuDropdown.bind(this);
+    this.onRefreshButtonClicked = this.onRefreshButtonClicked.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ isClient: true });
+  }
+
+  onToggleMenuDropdown() {
+    this.setState({ isMenuOpened: !this.state.isMenuOpened });
+  }
+
+  onRefreshButtonClicked() {
+    // TODO: refresh cache
+  }
+
+  renderMenu() {
+    return (
+      <Dropdown
+        pullRight
+        id="sidebarMenuDropdown"
+        className="sidebar-menu-dropdown"
+        open={this.state.isMenuOpened}
+        onToggle={this.onToggleMenuDropdown}
+      >
+
+        <Dropdown.Toggle bsSize="xs">
+          <i className="icon-settings small"></i>
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <MenuItem eventKey="1">
+            <i className="icon-reload" onClick={this.onRefreshButtonClicked}></i> Refresh
+          </MenuItem>
+        </Dropdown.Menu>
+
+      </Dropdown>
+    );
   }
 
   render() {
+    // render hydrated contents
+    if (!this.state.isClient) {
+      return (
+        <div key="sidebar" dangerouslySetInnerHTML={{ __html: this.state.html }}></div>
+      );
+    }
+
     return (
-      // eslint-disable-next-line react/no-danger
-      <div key="sidebar" dangerouslySetInnerHTML={{ __html: this.state.html }}></div>
+      <>
+        <div key="sidebar" dangerouslySetInnerHTML={{ __html: this.state.html }}></div>
+
+        { this.renderMenu() }
+      </>
     );
   }
 
